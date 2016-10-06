db99fbff468ab0e96b7b99c478faab0a702a2d3d
Steven Whitelock <g1eagle@gmail.com>
2016-10-06 15:25:09 -0500

    Redo and see user

--- RPIStart.sh
+++ RPIStart.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
+echo "$USER"
 adduser --disabled-password --gecos '' r
 adduser cowrie sudo
 echo '%sudo ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoers
+echo "import pty; pty.spawn('/bin/bash')" > /tmp/asdf.py
+python /tmp/asdf.py
 su -m r -c /cowrie/start.sh
